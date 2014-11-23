a8ffcedddb5ddcbd9b787dff383b442e96e6455a
Ran Snir <ransnir@gmail.com>
2014-11-23 14:03:02 +0200

    remove ALSystem

--- ALSystemUtilities.embeddedframework/ALSystemUtilities.framework/Versions/A/Headers/ALSystem_JailbreakConstants.h
+++
@@ -1,9 +0,0 @@
-//
-//  ALSystem_JailbreakConstants.h
-//  ALSystem
-//
-//  Created by Andrea Mario Lufino on 10/09/13.
-//  Copyright (c) 2013 Andrea Mario Lufino. All rights reserved.
-//
-
-static NSString *const ALJailbreak_isJailbroken = @"isJailbroken";

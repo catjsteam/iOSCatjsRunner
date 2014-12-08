ae51619db66faf7abb61d3aac4d7f36279242f91
Ran Snir <ransnir@gmail.com>
2014-12-08 14:08:08 +0200

    update screenshot

--- iPhone/CATRunner/Screenshot.h
+++ iPhone/CATRunner/Screenshot.h
@@ -6,8 +6,18 @@
 //  Copyright (c) 2014 Nadav Vanunu. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
 
-@interface Screenshot : NSObject
+@interface Screenshot : NSObject {
+    
+}
+
+- (void)sendPostWithScreenshot:(UIImage*)thisImage
+                      forScrap:(NSString*)scrapName
+                   forDeviceId:(NSString*)deviceId
+                  forServerUrl:(NSString*)baseUrl;
+
+- (UIImage *)getScreenshot;
 
 @end
--- iPhone/CATRunner/Screenshot.m
+++ iPhone/CATRunner/Screenshot.m
@@ -7,7 +7,58 @@
 //
 
 #import "Screenshot.h"
+#import "AFNetworking.h"
 
-@implementation Screenshot
+@implementation Screenshot {
+    
+}
+
+
+- (void)sendPostWithScreenshot:(UIImage*)thisImage
+                      forScrap:(NSString*)scrapName
+                   forDeviceId:(NSString*)deviceId
+                    forServerUrl:(NSString*)baseUrl
+
+{
+    
+    NSString *screenshotUrl = [NSString stringWithFormat:@"%@/screenshot", baseUrl];
+    NSLog(@"send post request to: %@", screenshotUrl);
+    
+    
+    UIDevice *deviceInfo = [UIDevice currentDevice];
+    NSData *imageData = UIImagePNGRepresentation(thisImage);
+    NSString *encodedString = [imageData base64Encoding];
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    NSDictionary *params = @{@"scrapName": scrapName,
+                             @"deviceName" : deviceInfo.name,
+                             @"deviceId" : deviceId,
+                             @"deviceType" : @"iOS",
+                             @"pic" : encodedString};
+    [manager POST:screenshotUrl parameters:params
+     
+          success:^(AFHTTPRequestOperation *operation, id responseObject) {
+              NSLog(@"JSON: %@", responseObject);
+          } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+              NSLog(@"Error: %@", error);
+          }];
+    
+    return;
+}
+
+
+
+- (UIImage *)getScreenshot
+{
+//    NSLog(@"try to take screenshot in new method");
+//    UIGraphicsBeginImageContextWithOptions(self.view.bounds.size, self.view.opaque, 0.0);
+//    [self.webView.layer renderInContext:UIGraphicsGetCurrentContext()];
+//    UIImage*theImage=UIGraphicsGetImageFromCurrentImageContext();
+//    
+//    UIGraphicsEndImageContext();
+//    
+//    return theImage;
+    return nil;
+}
 
 @end

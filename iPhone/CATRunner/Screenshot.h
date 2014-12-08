45467b1b0a839bdca338c03587b03fac20976762
Ran Snir <ransnir@gmail.com>
2014-12-08 14:06:52 +0200

    remove break points and add screenshot support

---
+++ iPhone/CATRunner/Screenshot.h
@@ -0,0 +1,13 @@
+//
+//  Screenshot.h
+//  CATRunner
+//
+//  Created by Ran Snir on 12/8/14.
+//  Copyright (c) 2014 Nadav Vanunu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Screenshot : NSObject
+
+@end
---
+++ iPhone/CATRunner/Screenshot.m
@@ -0,0 +1,13 @@
+//
+//  Screenshot.m
+//  CATRunner
+//
+//  Created by Ran Snir on 12/8/14.
+//  Copyright (c) 2014 Nadav Vanunu. All rights reserved.
+//
+
+#import "Screenshot.h"
+
+@implementation Screenshot
+
+@end
